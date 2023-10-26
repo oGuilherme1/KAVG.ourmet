342c8fdf7a7ade191b6a37895e8bdc50e0bf71a8
Guilherme Gonçalves <guilhermegoncalves1314@gmail.com>
2023-10-26 20:59:16 -0300

    Subindo template

--- KAVG.ourmet/routes/web.php
+++ KAVG.ourmet/routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/Produtos', function () {
+    return view('produto');
+})->middleware(['auth', 'verified'])->name('produtos');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
