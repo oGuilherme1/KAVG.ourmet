10d9f108504c263dbd20e353459048fd6a93f4b6
luishenriquesenai <0000939921@senaimgaluno.com.br>
2023-10-27 21:05:23 -0300

    listagem de todos os produtos

--- KAVG.ourmet/routes/web.php
+++ KAVG.ourmet/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,6 +22,8 @@ Route::get('/', function () {
 Route::get('/Produtos', function () {
     return view('produto');
 })->middleware(['auth', 'verified'])->name('produtos');
+
+Route::get('/products', [ProductController::class,'index'])->name('products');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
