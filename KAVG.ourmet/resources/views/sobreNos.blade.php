f69effcc7eab9f2cefb3938c5bac0d7a0aeadc45
davi <129806579+DaviSanXz@users.noreply.github.com>
2023-10-31 16:00:53 -0300

    footer

--- KAVG.ourmet/resources/views/sobreNos.blade.php
+++ KAVG.ourmet/resources/views/sobreNos.blade.php
@@ -1,43 +1,38 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!DOCTYPE html> <html lang="{{ str_replace('_', '-', app()->getLocale()) }}"> <head> <meta charset="utf-8"> <meta
+    name="viewport" content="width=device-width, initial-scale=1">
+@vite(['resources/css/app.css', 'resources/js/app.js'])
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+<title>Laravel</title>
 
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-</head>
+<!-- Fonts -->
+<link rel="preconnect" href="https://fonts.bunny.net">
+<link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" /> </head>
 
 <body class="antialiased">
     <div x-data="{ cartOpen: false , isOpen: false }" class="bg-off_white">
         @include('header_carrinho')
-        <div class="py-16 bg-white">  
-  <div class="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
-      <div class="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
-        <div class="md:5/12 lg:w-5/12">
-          <img src="{{ asset('img/logoRosaPng.png') }}" alt="image" loading="lazy" width="" height="">
-        </div>
-        <div class="md:7/12 lg:w-6/12">
-          <h2 class="text-2xl text-gray-900 font-bold md:text-4xl">Nuxt development is carried out by passionate developers</h2>
-          <p class="mt-6 text-gray-600">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis voluptatem accusantium nemo perspiciatis delectus atque autem! Voluptatum tenetur beatae unde aperiam, repellat expedita consequatur! Officiis id consequatur atque doloremque!</p>
-          <p class="mt-4 text-gray-600"> Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at? Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia.</p>
-        </div>
-      </div>
-  </div>
-</div>
+        <div class="py-16 bg-white">
+            <div class="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+                <div class="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
+                    <div class="md:5/12 lg:w-5/12">
+                        <img src="{{ asset('img/logoRosa.png') }}" alt="image" loading="lazy" width="" height="">
+                    </div>
+                    <div class="md:7/12 lg:w-6/12">
+                        <h2 class="text-2xl text-gray-900 font-bold md:text-4xl">Delicie-se com a doçura que vai
+                            encantar seus sentidos!</h2>
+                        <p class="mt-6 text-gray-600">Na nossa loja de doces, exploramos o mundo mágico dos sabores,
+                            onde cada mordida é uma jornada de alegria. Oferecemos uma seleção de doces artesanais e
+                            guloseimas irresistíveis que vão adoçar o seu dia. Dos clássicos aos mais inovadores, nossos
+                            doces são preparados com amor e criatividade. Venha descobrir um paraíso de sabor que vai
+                            fazer você sorrir a cada pedaço. Seja bem-vindo ao nosso mundo doce, onde a felicidade é o
+                            ingrediente secreto de cada criação.</p>
 
-        <footer class="bg-gray-200">
-            <div class="container mx-auto px-6 py-3 flex justify-between items-center">
-                <a href="#" class="text-xl font-bold text-gray-500 hover:text-gray-400">Brand</a>
-                <p class="py-2 text-gray-500 sm:py-0">All rights reserved</p>
+                    </div>
+                </div>
             </div>
-        </footer>
+        </div>
+
+        @include('footer')
     </div>
 </body>
 
